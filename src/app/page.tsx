1dc1b5766d0776260fef8123085147f431e2a561
AtulMinz <atulminz.jsr@gmail.com>
2024-03-13 20:49:39 +0530

    Few tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { IoSearch } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <main className="flex justify-center p-24 bg-gradient-to-b from-[#0F2167] via-blue-900 to-blue-700 h-[100vh]">
+    <main className="flex justify-center p-32 bg-gradient-to-b from-[#0F2167] via-blue-900 to-blue-700 h-[100vh]">
       <div className="flex flex-col pt-20 items-center">
         <h1 className="bg-clip-text text-4xl font-semibold text-[#8696FE]">
           Welcome to Educative. Get hands-on with in-demand tech skills.
@@ -15,11 +15,11 @@ export default function Home() {
         </div>
         <br></br>
         <div className="flex justify-center items-center border-2 border-gray-500 w-3/6 h-14 hover:bg-blue-950 rounded-lg">
-          <button className="mr-5 bg-white">
-            <IoSearch className="" />
+          <button className="mr-5 pr-3">
+            <IoSearch className="fill-white" />
           </button>
           <input
-            className="mt-1 bg-inherit w-4/5 h-1/2 mb-2"
+            className="mt-1 bg-inherit w-4/5 h-1/2 mb-2 focus:outline-none text-white"
             type="text"
             placeholder="Search for courses."
           />
