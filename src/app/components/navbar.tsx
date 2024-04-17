af71ed5ee0c19a49327796f4d9550b0bef59ca6b
AtulMinz <atulminz.jsr@gmail.com>
2024-04-18 02:07:58 +0530

    Major changes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,13 +3,21 @@
 
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useEffect } from "react";
 
 export default function Navbar() {
   const session = useSession();
   const router = useRouter();
+
   return (
     <NavigationMenu.Root className="flex justify-around p-3 text-slate-200 gap-3">
       <NavigationMenu.List>
@@ -29,9 +37,25 @@ export default function Navbar() {
         </NavigationMenu.Item>
       </NavigationMenu.List>
       {session ? (
-        <span>
-          <img src={session.data?.user?.image || ""} alt="profile" />
-        </span>
+        <>
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="ghost">
+                <Avatar>
+                  <AvatarImage src={session.data?.user?.image || ""} />
+                  <AvatarFallback className="text-black">
+                    {session.data?.user?.name}
+                  </AvatarFallback>
+                </Avatar>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="bg-transparent border-none w-fit shadow-none p-0">
+              <span>
+                <Button onClick={() => signOut()}>Logout</Button>
+              </span>
+            </PopoverContent>
+          </Popover>
+        </>
       ) : (
         <Button
           className="gap-1 bg-blue-800 hover:bg-blue-900"
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,14 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 export default function Signin() {
   return (
-    <main>
-      <div>
-        <div>
-          <Button onClick={() => signIn("google")}>Signin</Button>
+    <main className="bg-[#e9ecef] min-h-screen flex justify-center items-center">
+      <div className="bg-[#ffffff] w-4/5 h-[80vh] flex flex-row rounded-md">
+        <div className="w-1/2">
+          <img
+            src="https://images.unsplash.com/photo-1553877522-43269d4ea984?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="background"
+            className="h-full"
+          />
+        </div>
+        <div className="w-1/2 flex justify-center items-center">
+          <div className="">
+            <Button
+              onClick={() => signIn("google")}
+              className="flex space-x-2 bg-[#ced4da] text-black"
+            >
+              <span>
+                <Image src="google.svg" alt="signin" width={15} height={5} />
+              </span>
+              <span>Signin</span>
+            </Button>
+          </div>
         </div>
       </div>
     </main>
