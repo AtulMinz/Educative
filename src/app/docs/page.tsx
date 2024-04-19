3df7136bf70a080394301c9987f9d2e08fbd2b37
AtulMinz <atulminz.jsr@gmail.com>
2024-04-19 23:17:00 +0530

    Docs section

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <main className="bg-gradient-to-b from-black to-slate-900 h-full w-full">
       <div className="pt-10">
-        <h1 className="text-white">Docs</h1>
+        <h1 className="text-white">Docs Educative</h1>
       </div>
     </main>
   );
