0e5f229fbdfe7b527a917f5e6a2ef29f6c7b27dc
AtulMinz <atulminz.jsr@gmail.com>
2024-04-28 13:42:41 +0530

    Added server action

--- src/app/actions/createUser.ts
+++ src/app/actions/createUser.ts
@@ -1,19 +1,20 @@
 "use server";
-import { NextResponse } from "next/server";
-import { hash } from "bcrypt";
+
 import db from "@/lib/client";
 import { redirect } from "next/navigation";
+import { hash } from "bcrypt";
 
 export async function createUser(data: FormData) {
   const name = data.get("name") as string;
   const email = data.get("email") as string;
   const password = data.get("password") as string;
+  const hashedPassword = await hash(password, 10);
 
   const newUser = await db.user.create({
     data: {
       name: name,
       email: email,
-      password: password,
+      password: hashedPassword,
     },
   });
   console.log(newUser);
